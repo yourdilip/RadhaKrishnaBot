0402e8030a04d18420e6bb061616213d4837a5be
Bhimsinh Sodha <119826787+bjsodha@users.noreply.github.com>
2023-10-14 13:05:11 +0530

    Fix

--- plugins/commands.py
+++ plugins/commands.py
@@ -286,8 +286,6 @@ async def start(client, message):
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
-           
-    """Send basic information of channel"""
     if isinstance(CHANNELS, (int, str)):
         channels = [CHANNELS]
     elif isinstance(CHANNELS, list):
